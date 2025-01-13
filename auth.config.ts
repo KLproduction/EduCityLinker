648f98d045442e6b5b042d7837655bfc31c241dc
KentLaw <kent1988hk@gmail>
2025-01-13 17:12:54 +0000

    authConfig updated

--- auth.config.ts
+++ auth.config.ts
@@ -8,34 +8,28 @@ import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "./data/user";
 
 export default {
-    
   providers: [
-    GitHub,
+    GitHub({
+      clientId: process.env.AUTH_GITHUB_ID,
+      clientSecret: process.env.AUTH_GITHUB_SECRET,
+    }),
     Google,
     Credentials({
-        authorize: async(credentials)=>{
+      authorize: async (credentials) => {
+        const validatedFields = LoginSchema.safeParse(credentials);
 
-            const validatedFields = LoginSchema.safeParse(credentials);
-    
-            if (validatedFields.success){
-                const { email , password} = validatedFields.data;
-    
-                const user = await getUserByEmail(email);
-                if(!user || !user.password) return null
-    
-                const passwordMatch = await bcrypt.compare(
-                    password,
-                    user.password,
-                )
-                if (passwordMatch) return user;
-            };
-    
-            return null;
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+          if (passwordMatch) return user;
         }
-        
 
-        
-    })
-
+        return null;
+      },
+    }),
   ],
 } satisfies NextAuthConfig;
